07325686ed173dd3925402d0cb734e2f9c32a747
Gaurav Kalbhor <121425278+GauravKalbhor@users.noreply.github.com>
2023-09-17 16:11:14 +0530

    Add files via upload

---
+++ assects/js/login.js
@@ -0,0 +1,19 @@
+function login(){
+  let login = document.querySelector(".login1")
+  if(login.style.display == "none"){
+    login.style.display = "flex"
+  }
+  else{
+    login.style.display = "none"
+  }
+}
+
+function signin(){
+  let signin = document.querySelector(".login1")
+  if(signin.style.display == "none"){
+    signin.style.display = "flex"
+  }
+  else{
+    signin.style.display = "none"
+  }
+}
